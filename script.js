9f7f0e7df99cd2e9fb18ef900d749b46038fd37c
Tcharlles <tcharlles3@gmail.com>
2022-05-15 12:50:38 -0300

    desktop-ok

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function eventosaulas(aula) {
 }
 
 aulas.forEach(eventosaulas);
+
+//FAQ
+
+const perguntas = document.querySelectorAll(".faq button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
